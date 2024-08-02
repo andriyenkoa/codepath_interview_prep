7b75bcdfe43ab7a5220fd63d5344cb9d24eecdaf
Andrey Andriyenko <amandriyenko@gmail.com>
2024-08-02 17:35:07 +0500

    Container With Most Water

---
+++ Session 5,2/container_with_most_water.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        result = 0
+        while left < right:
+            temp_res = min(height[left], height[right]) * (right - left)
+            result = max(result, temp_res)
+            if height[left] < height[right]:
+                left += 1
+            elif height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+                right -= 1
+        return result

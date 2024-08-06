b3609e55eec125aa66bbbec1138b6192510a2dc5
Andrey Andriyenko <amandriyenko@gmail.com>
2024-08-06 15:08:52 +0500

    Kth Smallest Element in a BST

--- Session 7,2/kth_smallest_element_in_a_bst.py
+++ Session 7,2/kth_smallest_element_in_a_bst.py
@@ -23,8 +23,30 @@ class Solution:
             temp += 1
         return heapq.heappop(self.heap)
 
-    def insert_to_heap(self,node):
+    def insert_to_heap(self, node):
         if node:
             heapq.heappush(self.heap, node.val)
             self.insert_to_heap(node.left)
             self.insert_to_heap(node.right)
+
+    # in-order traversal solution
+    def kthSmallest2(self, root: Optional[TreeNode], k: int) -> int:
+        def helper(node):
+            nonlocal kthNode
+            nonlocal k
+
+            if not node or k < 0:
+                return
+
+            helper(node.left)
+
+            k -= 1
+            if k == 0:
+                kthNode = node.val
+                return
+
+            helper(node.right)
+
+        kthNode = 0
+        helper(root)
+        return kthNode
